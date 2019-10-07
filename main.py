984f52c94824a1f80511a8294e19d3ceba20a0b3
Nick Spence <nick@triceimaging.com>
2019-10-07 11:03:43 -0700

    full sunrise with color transition

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import machine
 from machine import Pin
 from neopixel import NeoPixel
-import time
+import utime
 import urequests
 
 STEP_MS = 250
@@ -9,8 +9,8 @@ NEOPIXEL_PIN = 2   # set GPIO2 (D4) to output to drive NeoPixels
 NUM_PIXELS = 16
 SUNRISE_DURATION = 300.0
 SUNRISE_DURATION_MS = 300 * 1000.0
-SUNRISE_COLORS_START = [(255, 190, 90), (227, 114, 173), (144, 132, 219)] # from center out
-SUNRISE_COLORS_END = [(255, 190, 90), (227, 114, 173)] # from center out
+SUNRISE_COLORS_START = [(255, 176, 59), (227, 114, 173), (144, 132, 219)] # from center out
+SUNRISE_COLORS_END = [(255, 190, 90), (255, 183, 59)] # from center out
 
 class ColorPoint:
     def __init__(self, index, color):
@@ -32,36 +32,40 @@ class Pixel:
     # state is between 0 and 1
     def color_at(self, state):
         color = tuple([(self.end_color[index] - v) * state + v for index, v in enumerate(self.start_color)])
-        return tuple([int(v * state * self.full_luminosity) for v in color])
+        return tuple([self._normalize_color_value(v * state * self.full_luminosity) for v in color])
+
+    def _normalize_color_value(self, v):
+        return max(min(int(v), 255), 0)
+
 
 class Sky:
     def __init__(self, num_pixels, start_colors, end_colors):
         self.num_pixels = num_pixels
         self.start_colors = start_colors
         self.end_colors = end_colors
-        self.lum_multipliers = _lum_multplier_parabolic(num_pixels)
-        self.start_gradient = _mirrored_color_gradient(start_colors)
-        self.end_gradient = _mirrored_color_gradient(end_colors)
-        self.pixels = [Pixel(start_gradient[i], end_color=end_gradient[i], full_luminosity=lum_multipliers[i]) for i in range(num_pixels)]
+        self.lum_multipliers = self._lum_multplier_parabolic(self.num_pixels)
+        self.start_gradient = self._mirrored_color_gradient(self.start_colors)
+        self.end_gradient = self._mirrored_color_gradient(self.end_colors)
+        self.pixels = [Pixel(self.start_gradient[i], end_color=self.end_gradient[i], full_luminosity=self.lum_multipliers[i]) for i in range(self.num_pixels)]
 
     def colors_at(self, state):
-        return [pixel.color_at(state) for pixel in pixels]
+        return [pixel.color_at(state) for pixel in self.pixels]
 
     def _lum_multplier_parabolic(self, n):
         midpoint = (n-1) / 2.0
         parabolic = lambda midpoint, x: -1 * ((x-midpoint)**2)/(midpoint**2.0) + 1.1
-        return [_clip(parabolic(midpoint, i)) for i in range(n)]
+        return [min(1, parabolic(midpoint, i)) for i in range(n)]
 
     def _clip(self, color):
         tuple([max(min(v, 255), 0) for v in color])
 
     def _mirrored_color_gradient(self, colors, midpoint=None):
         colors = colors[::-1] + colors
-        midpoint = midpoint or (num_pixels - 1) / 2.0
-        color_points = [ColorPoint(index / float(len(colors) - 1) * (num_pixels - 1), color) for index, color in enumerate(colors)]
+        midpoint = midpoint or (self.num_pixels - 1) / 2.0
+        color_points = [ColorPoint(index / float(len(colors) - 1) * (self.num_pixels - 1), color) for index, color in enumerate(colors)]
 
         results = []
-        for i in range(num_pixels):
+        for i in range(self.num_pixels):
             color_point1 = None
             color_point2 = None
             for cp in color_points:
@@ -72,31 +76,27 @@ class Sky:
                     break
             difference = color_point2.index - color_point1.index
             ratio = 1 if difference == 0 else (i - color_point1.index) / difference
-            results.append(_merge_colors_weighted(color_point1.color, color_point2.color, ratio))
+            results.append(self._merge_colors_weighted(color_point1.color, color_point2.color, ratio))
         return results
 
     def _merge_colors_weighted(self, color1, color2, ratio):
         return tuple([(color2[index] - v) * ratio + v for index, v in enumerate(color1)])
 
-
 def main():
     pin = Pin(NEOPIXEL_PIN, Pin.OUT)
     np = NeoPixel(pin, NUM_PIXELS)
-    #  colors = mirrored_color_gradient(NUM_PIXELS, colors=SUNRISE_COLORS)
-    #  lum_multipliers = lum_multplier_parabolic(NUM_PIXELS)
     sky = Sky(NUM_PIXELS, SUNRISE_COLORS_START, SUNRISE_COLORS_END)
 
-    start_time = time.tick_ms()
+    start_time = utime.ticks_ms()
     elapsed_ms = 0
 
     while elapsed_ms < SUNRISE_DURATION_MS:
         display_values = sky.colors_at(elapsed_ms / SUNRISE_DURATION_MS)
-        #  display_values = apply_single_lum_multiplier(apply_lum_array(colors, lum_multipliers), elapsed_ms / SUNRISE_DURATION_MS)
         print(display_values)
         display_pixels(np, display_values)
 
-        elapsed_ms = time.ticks_diff(time.ticks_ms(), start_time)
-        time.sleep_ms(STEP_MS)
+        elapsed_ms = utime.ticks_diff(utime.ticks_ms(), start_time)
+        utime.sleep_ms(STEP_MS)
 
 def get_sky_info():
     response = urequests.get("https://api.sunrise-sunset.org/json?lat=47.2529&lng=122.4443&formatted=0")
@@ -104,20 +104,11 @@ def get_sky_info():
         return None
     response.json()['results']
 
-#  def apply_single_lum_multiplier(rgb_array, luminosity):
-    #  return [apply_lum_multiplier(pixel, luminosity) for pixel in rgb_array]
-
-#  def apply_lum_array(rgb_array, lum_array):
-    #  return [apply_lum_multiplier(pixel, lum_array[index]) for index, pixel in enumerate(rgb_array)]
-
-#  def apply_lum_multiplier(pixel, lum_value):
-    #  return tuple([v * lum_value for v in pixel])
-
 def normalize_float_color(pixel):
-    return tuple([int(v) for v in pixel])
+    return tuple([max(min(int(v), 255), 0) for v in pixel])
 
 def display_pixels(neo_pixel, rgb_array):
-    for color in rgb_array:
+    for i, color in enumerate(rgb_array):
       #  neo_pixel[i] = normalize_float_color(rgb_array[i])
       neo_pixel[i] = color
     neo_pixel.write()
